fc13d094a19c7cbddb23656d53daff875fbf6f0d
Prince Donda <princedonda4489@gmail.com>
2024-09-11 08:28:55 +0530

    MVC
    start with mvc

---
+++ Project-01/models/user.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+//Schema
+const userSchema = new mongoose.Schema({
+    firstname:{
+        type: String,
+        required: true,
+    },
+    lastname:{
+        type: String,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    jobtitle:{
+        type: String,
+    },
+    gender:{
+        type: String,
+        required: true,
+        enum: ['Male', 'Female', 'Other'],
+    },
+},
+{timestamps:true},
+);
+
+//model
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
